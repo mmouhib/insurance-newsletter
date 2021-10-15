79bbebf7bc95b56e3b37ff8846742ce3888a2f9a
mouhib <59750643+mmouhib@users.noreply.github.com>
2021-10-15 17:32:19 +0100

    added mail sending requirements

--- src/website/index.py
+++ src/website/index.py
@@ -2,15 +2,16 @@ from flask import Blueprint, render_template, request
 
 index = Blueprint('index', __name__)
 
-@index.route('/', methods = ['GET', 'POST'])
+
+@index.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         object = request.form.get('object')
         content = request.form.get('content')
         emails = request.form.get('emails')
-    print(object, content, emails)
 
     return render_template('index.html')
+
 
 @index.route('/success')
 def success():
