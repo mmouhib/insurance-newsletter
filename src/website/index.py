2e2c6d6230575e2c987cb25201594622ef7d29b8
mouhib <59750643+mmouhib@users.noreply.github.com>
2021-10-16 10:08:57 +0100

    added file submision support

--- src/website/index.py
+++ src/website/index.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from src.website.static import sender
+import os
 
 index = Blueprint('index', __name__)
+
+print(os.getcwd())
 
 
 @index.route('/', methods=['GET', 'POST'])
@@ -10,6 +13,19 @@ def home():
         subject = request.form.get('object')
         content = request.form.get('content')
         emails = request.form.get('emails')
+
+        if request.files:
+            file = request.files['file']
+            cwd = os.getcwd() + r"\website\static\uploads"
+            file.save(os.path.join(cwd, file.filename))
+
+            if file.filename.rsplit('.', 1)[1].lower() == 'txt':
+                filetype = 'text'
+            else:
+                filetype = 'excel'
+
+
+
         sender.mail_sending(subject, content, emails)
         return redirect(url_for('index.success'))
     return render_template('index.html')
---
+++ src/website/static/index.js
@@ -0,0 +1,4 @@
+document.body.addEventListener('click', function (){
+    let fileup = document.getElementsByClassName('form-control')[0]
+    console.log(fileup.value)
+})
